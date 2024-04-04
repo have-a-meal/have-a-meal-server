98521e496c65bb2f28743045cdfa7a77262082f0
JaeUng <wodndrnjs@naver.com>
2024-04-04 16:28:42 +0900

    :tada: Initial Server #EAT-42

---
+++ src/main/java/team/gwon/haveameal/HaveAMealServerApplication.java
@@ -0,0 +1,11 @@
+package team.gwon.haveameal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HaveAMealServerApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(HaveAMealServerApplication.class, args);
+	}
+}
