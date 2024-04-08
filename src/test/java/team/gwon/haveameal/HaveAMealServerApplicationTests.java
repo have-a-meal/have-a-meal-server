d113db55d5c83ecc5e4e23f5ad041aa9fe7b1288
wonjun <vcfdxzsa12@gmail.com>
2024-04-08 23:27:39 +0900

    (test) : 테스트 환경 분리 #EAT-20 코드 리뷰 후 unit test를 목적으로 하는 테스트 코드 분리

--- src/test/java/team/gwon/haveameal/HaveAMealServerApplicationTests.java
+++ src/test/java/team/gwon/haveameal/HaveAMealServerApplicationTests.java
@@ -1,23 +1,12 @@
 package team.gwon.haveameal;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.mock.web.MockMultipartFile;
-import org.springframework.web.multipart.MultipartFile;
-import team.gwon.haveameal.excelExtract.controller.ExcelExtractController;
-
-import java.io.FileInputStream;
 
 @SpringBootTest
 class HaveAMealServerApplicationTests {
 
-    @Autowired
-    public ExcelExtractController excelExtractController;
-
-    @Test
-    void contextLoads() throws Exception {
-        MultipartFile multipartFile = new MockMultipartFile("test.xlsx", new FileInputStream("D:\\extest.xlsx"));
-        excelExtractController.ExcelRead(multipartFile);
-    }
+	@Test
+	void contextLoads() {
+	}
 }
---
+++ src/test/java/team/gwon/haveameal/unitexcel/ExcelReadTest.java
@@ -0,0 +1,27 @@
+package team.gwon.haveameal.unitexcel;
+
+import java.io.FileInputStream;
+import java.util.List;
+import java.util.Map;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.web.multipart.MultipartFile;
+
+import lombok.extern.slf4j.Slf4j;
+import team.gwon.haveameal.excelextract.service.ExcelExtractService;
+
+@Slf4j
+public class ExcelReadTest {
+	public ExcelExtractService excelExtractService;
+
+	@Test
+	void testReadExcel() throws Exception {
+		excelExtractService = new ExcelExtractService();
+		MultipartFile multipartFile = new MockMultipartFile("test.xlsx", new FileInputStream("D:\\4월 1주차 식단표.xlsx"));
+		List<Map<String, Object>> testData = excelExtractService.excelUpload(multipartFile);
+		for (Map<String, Object> data : testData) {
+			log.info(data.toString());
+		}
+	}
+}
