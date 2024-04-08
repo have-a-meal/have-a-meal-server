f029b4647161917536e8bd08bb8f5c6d73427980
wjdwnsdnjs13 <67488973+wjdwnsdnjs13@users.noreply.github.com>
2024-04-08 17:14:26 +0900

    (test) : 결제 서비스 test 작성

---
+++ src/test/java/team/gwon/haveameal/PaymentServiceTest.java
@@ -0,0 +1,16 @@
+package team.gwon.haveameal;
+
+import org.junit.jupiter.api.Test;
+
+class PaymentServiceTest {
+
+	@Test
+	void 결제요청() {
+		final PaymentRequest request = new PaymentRequest();
+
+	}
+
+	private class PaymentRequest {
+
+	}
+}
