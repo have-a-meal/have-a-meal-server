37b402c849f8f8443e8f349769bd78c235aea77f
Geonwoo0423 <hyunsung0423@naver.com>
2024-04-08 16:59:17 +0900

    (refactor) : 회원가입을 위한 DTO #EAT-37 회원가입 시 필요한 변수들만 선언한 DTO 생성

---
+++ src/main/java/team/gwon/haveameal/register/domain/MemberRegister.java
@@ -0,0 +1,13 @@
+package team.gwon.haveameal.register.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class MemberRegister {
+	private String memberId;
+	private String password;
+	private String name;
+	private String phone;
+}
