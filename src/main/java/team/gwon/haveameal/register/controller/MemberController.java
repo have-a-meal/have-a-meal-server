93c7b84f353c5b257aa86ecbcedb4cbb5c089151
Geonwoo0423 <hyunsung0423@naver.com>
2024-04-08 16:51:14 +0900

    (refactor) : 패키지 구분 및 생성자 사용 #EAT-37 기능별 패키지 구분, Autowired 제거 후 생성자를 사용하여 의존성 주입

--- src/main/java/team/gwon/haveameal/register/controller/MemberController.java
+++ src/main/java/team/gwon/haveameal/register/controller/MemberController.java
@@ -1,20 +1,22 @@
-package team.gwon.haveameal.controller;
+package team.gwon.haveameal.register.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import team.gwon.haveameal.domain.Member;
-import team.gwon.haveameal.service.MemberService;
+import team.gwon.haveameal.register.domain.MemberRegister;
+import team.gwon.haveameal.register.service.MemberService;
 
 @RestController
 public class MemberController {
-	@Autowired
 	private MemberService memberService;
 
+	public MemberController(MemberService memberService) {
+		this.memberService = memberService;
+	}
+
 	@PostMapping("/member")
-	public void insertMember(@RequestBody Member member) {
+	public void insertMember(@RequestBody MemberRegister member) {
 		memberService.insertMember(member);
 	}
 
