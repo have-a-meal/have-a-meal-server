9af3043f9e5432da934373c4ce9e8770f633a6b4
geonwoo0423 <hyunsung0423@naver.com>
2024-04-11 16:02:29 +0900

    (refactor) : 어노테이션 제거 #EAT-37

--- src/main/java/team/gwon/haveameal/member/mapper/MemberMapper.java
+++ src/main/java/team/gwon/haveameal/member/mapper/MemberMapper.java
@@ -1,12 +1,10 @@
 package team.gwon.haveameal.member.mapper;
 
 import org.apache.ibatis.annotations.Mapper;
-import org.springframework.stereotype.Repository;
 
-import team.gwon.haveameal.member.domain.MemberRegister;
+import team.gwon.haveameal.member.domain.MemberRegisterDto;
 
-@Repository
 @Mapper
 public interface MemberMapper {
-	void insertMember(MemberRegister member);
+	void insertMember(MemberRegisterDto member);
 }
