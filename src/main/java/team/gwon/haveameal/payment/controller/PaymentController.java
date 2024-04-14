baf21d2d6389b8e6e96a0a2e77ee81176cecd2f4
wjdwnsdnjs13 <67488973+wjdwnsdnjs13@users.noreply.github.com>
2024-04-14 16:27:56 +0900

    (feat) : 결제 확인

--- src/main/java/team/gwon/haveameal/payment/controller/PaymentController.java
+++ src/main/java/team/gwon/haveameal/payment/controller/PaymentController.java
@@ -1,4 +1,42 @@
 package team.gwon.haveameal.payment.controller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RequestMapping("/payment")
+@RestController
+@RequiredArgsConstructor
+@PropertySource("classpath:/config/application-portone.properties")
 public class PaymentController {
+
+	@Value("${portone.restApiKey:null}")
+	private String restApiKey;
+
+	@Value("${portone.restApiSecret:null}")
+	private String restApiSecret;
+
+	@Value("${portone.noValue:null}")
+	private String noValue;
+
+	// private final PaymentService paymentService;
+
+	@GetMapping("/verify/{imp_uid}")
+	public void paymentByImpUid() {
+
+	}
+
+	@GetMapping("/test")
+	public void test() {
+		log.info("restApiKey : " + restApiKey);
+		log.info("restApiSecret : " + restApiSecret);
+		log.info("noValue : " + noValue.equals("null"));
+		log.info("noValue : " + (noValue == null ? "null" : "not null"));
+	}
 }
