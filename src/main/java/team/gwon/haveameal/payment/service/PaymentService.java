a6d88d9674297999450217e65dafea594f0a9580
wjdwnsdnjs13 <67488973+wjdwnsdnjs13@users.noreply.github.com>
2024-04-15 21:58:44 +0900

    (feat) : portOne 결제 정보 확인

--- src/main/java/team/gwon/haveameal/payment/service/PaymentService.java
+++ src/main/java/team/gwon/haveameal/payment/service/PaymentService.java
@@ -1,6 +1,12 @@
 package team.gwon.haveameal.payment.service;
 
+import java.io.IOException;
+
+import com.siot.IamportRestClient.exception.IamportResponseException;
+
 public interface PaymentService {
 
 	void test();
+
+	void verifyPayment(String impUid) throws IamportResponseException, IOException;
 }
