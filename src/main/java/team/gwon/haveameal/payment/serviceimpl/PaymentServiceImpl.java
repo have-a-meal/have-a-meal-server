7794ca4588c8f9f0f412c97095477064408a3a0a
wjdwnsdnjs13 <67488973+wjdwnsdnjs13@users.noreply.github.com>
2024-04-15 21:59:43 +0900

    (feat) : portOne 결제 정보 확인

--- src/main/java/team/gwon/haveameal/payment/serviceimpl/PaymentServiceImpl.java
+++ src/main/java/team/gwon/haveameal/payment/serviceimpl/PaymentServiceImpl.java
@@ -1,12 +1,23 @@
 package team.gwon.haveameal.payment.serviceimpl;
 
+import java.io.IOException;
+
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Service;
 
+import com.siot.IamportRestClient.IamportClient;
+import com.siot.IamportRestClient.exception.IamportResponseException;
+import com.siot.IamportRestClient.response.IamportResponse;
+import com.siot.IamportRestClient.response.Payment;
+
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import team.gwon.haveameal.payment.service.PaymentService;
 
 @Slf4j
+@RequiredArgsConstructor
+@PropertySource("classpath:/config/application-portone.properties")
 @Service
 public class PaymentServiceImpl implements PaymentService {
 
@@ -25,5 +36,15 @@ public class PaymentServiceImpl implements PaymentService {
 		log.info("restApiSecret : " + restApiSecret);
 		log.info("noValue : " + noValue.equals("null"));
 		log.info("noValue : " + (noValue == null ? "null" : "not null"));
+	}
+
+	@Override
+	public void verifyPayment(String impUid) throws IamportResponseException, IOException {
+		log.info("portOne api 연동 확인 : " + iamportClient);
+		IamportResponse<Payment> iamportResponse = iamportClient.paymentByImpUid(impUid);
+		Long amount = iamportResponse.getResponse().getAmount().longValue(); // 결제 금액
+		String name = iamportResponse.getResponse().getName(); // 상품명
+		String status = iamportResponse.getResponse().getStatus(); //결제 상태
+		log.info("데이터 확인 : , amount = {}, name = {}, status = {}", amount, name, status);
 	}
 }
