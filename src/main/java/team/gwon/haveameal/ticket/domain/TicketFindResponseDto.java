a90a2679f58a5f3a33be9d46b9a835094366d307
JaeUng <wodndrnjs@naver.com>
2024-04-17 19:55:35 +0900

    (feat) : Dto to Entity Method 추가

--- src/main/java/team/gwon/haveameal/ticket/domain/TicketFindResponseDto.java
+++ src/main/java/team/gwon/haveameal/ticket/domain/TicketFindResponseDto.java
@@ -3,16 +3,18 @@ package team.gwon.haveameal.ticket.domain;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import team.gwon.haveameal.payment.entity.Course;
+import team.gwon.haveameal.payment.entity.PaymentWithCourseIncludeDetail;
 
 @Getter
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class TicketFindResponseDto {
-	private String paymentId;
 	private String timing;
 	private String courseType;
-	private int price;
+	private int quantity;
 
-	public static TicketFindResponseDto of(String paymentId, String timing, String courseType, int price) {
-		return new TicketFindResponseDto(paymentId, timing, courseType, price);
+	public static TicketFindResponseDto of(PaymentWithCourseIncludeDetail payment) {
+		Course course = payment.getCourse();
+		return new TicketFindResponseDto(course.getTiming(), course.getCourseType(), payment.getQuantity());
 	}
 }
