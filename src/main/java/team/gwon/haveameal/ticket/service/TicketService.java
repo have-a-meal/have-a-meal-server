42501c945ba1c67b8a0d5c16b303782c8e4bbc84
JaeUng <wodndrnjs@naver.com>
2024-04-18 01:09:19 +0900

    (feat) : TicketService 추가

--- src/main/java/team/gwon/haveameal/ticket/service/TicketService.java
+++ src/main/java/team/gwon/haveameal/ticket/service/TicketService.java
@@ -6,21 +6,24 @@ import java.util.List;
 import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import team.gwon.haveameal.payment.entity.PaymentWithCourseIncludeDetail;
 import team.gwon.haveameal.ticket.domain.TicketFindRequestDto;
 import team.gwon.haveameal.ticket.domain.TicketFindResponseDto;
 import team.gwon.haveameal.ticket.mapper.TicketMapper;
 
+@Slf4j
 @RequiredArgsConstructor
 @Service
 public class TicketService {
 	private final TicketMapper ticketMapper;
 
 	public List<TicketFindResponseDto> findAllTickets(TicketFindRequestDto ticketFindRequestDto) {
-		List<PaymentWithCourseIncludeDetail> tickets = ticketMapper.findAllMyTickets(
-			TicketFindRequestDto.from(ticketFindRequestDto)).orElseThrow(RuntimeException::new);
-		// CustomException 추가 예정.
 
+		List<PaymentWithCourseIncludeDetail> tickets = ticketMapper.findAllMyTickets(
+			TicketFindRequestDto.from(ticketFindRequestDto));
+		// CustomException 추가 예정.
+		log.info("tickets : {}", tickets);
 		List<TicketFindResponseDto> response = new ArrayList<>();
 		for (PaymentWithCourseIncludeDetail paymentWithCourseIncludeDetail : tickets) {
 			response.add(TicketFindResponseDto.of(paymentWithCourseIncludeDetail));
