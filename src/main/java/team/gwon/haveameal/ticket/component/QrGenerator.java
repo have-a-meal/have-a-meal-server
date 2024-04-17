988a0a80cba62558712add75a87aa1fe1d816614
JaeUng <wodndrnjs@naver.com>
2024-04-17 16:41:22 +0900

    (feat) : QrGenerator 추가 토큰값을 기반으로 QR 코드 생성 이미지를 리턴

--- src/main/java/team/gwon/haveameal/ticket/component/QrGenerator.java
+++ src/main/java/team/gwon/haveameal/ticket/component/QrGenerator.java
@@ -11,11 +11,13 @@ import com.google.zxing.WriterException;
 import com.google.zxing.client.j2se.MatrixToImageWriter;
 import com.google.zxing.common.BitMatrix;
 
+import team.gwon.haveameal.common.util.TokenProvider;
+
 @Component
 public class QrGenerator {
 
-	public byte[] generateQrImage(String uuid, String api, int width, int height) throws WriterException, IOException {
-		String content = api + "/" + uuid;
+	public byte[] generateQrImage(int width, int height) throws WriterException, IOException {
+		String content = TokenProvider.generateToken().getAccessToken();
 		BitMatrix bitMatrix = new MultiFormatWriter()
 			.encode(content, BarcodeFormat.QR_CODE, width, height);
 		ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
