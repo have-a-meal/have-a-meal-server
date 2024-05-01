90396a24755ffb35636ed7bf21be37bbdb527e87
geonwoo0423 <hyunsung0423@naver.com>
2024-05-01 13:45:42 +0900

    (feat) : 개인정보 암호화 추가 #EAT-38

--- src/main/java/team/gwon/haveameal/member/service/MemberService.java
+++ src/main/java/team/gwon/haveameal/member/service/MemberService.java
@@ -4,7 +4,8 @@ import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
 import team.gwon.haveameal.member.domain.MemberRegisterDto;
-import team.gwon.haveameal.member.encryptionservice.EncryptionService;
+import team.gwon.haveameal.member.encryptionservice.password.PasswordEncryptor;
+import team.gwon.haveameal.member.encryptionservice.personaldata.PersonalDataEncryptor;
 import team.gwon.haveameal.member.mapper.MemberMapper;
 
 @Service
@@ -13,11 +14,18 @@ public class MemberService {
 
 	private final MemberMapper memberMapper;
 
-	private final EncryptionService encryptionService;
+	private final PasswordEncryptor passwordEncryptor;
+	private final PersonalDataEncryptor personalDataEncryptor;
 
 	public void insertMember(MemberRegisterDto member) {
-		String encryptedPassword = encryptionService.encryptPassword(member.getPassword());
+		String encryptedPassword = passwordEncryptor.encryptPassword(member.getPassword());
 		member.setPassword(encryptedPassword);
+		String encryptedId = personalDataEncryptor.encryptData(member.getMemberId());
+		member.setMemberId(encryptedId);
+		String encryptedName = personalDataEncryptor.encryptData(member.getName());
+		member.setName(encryptedName);
+		String encryptedPhone = personalDataEncryptor.encryptData(member.getPhone());
+		member.setPhone(encryptedPhone);
 
 		memberMapper.insertMember(member);
 	}
