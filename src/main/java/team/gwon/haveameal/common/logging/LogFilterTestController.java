b6a7ae2c6e33a19aabff98fbacc4360121c53da3
wjdwnsdnjs13 <67488973+wjdwnsdnjs13@users.noreply.github.com>
2024-05-01 14:42:14 +0900

    (feat) : LogFilter Get 통신 테스트를 위한 API 추가

--- src/main/java/team/gwon/haveameal/common/logging/LogFilterTestController.java
+++ src/main/java/team/gwon/haveameal/common/logging/LogFilterTestController.java
@@ -1,4 +1,15 @@
 package team.gwon.haveameal.common.logging;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import lombok.extern.slf4j.Slf4j;
+@Slf4j
+@RestController
+@RequestMapping("test")
 public class LogFilterTestController {
+	@GetMapping("/logTest")
+	public ResponseEntity<?> getLogTest() {
+		return ResponseEntity.status(HttpStatus.OK).body("정상적으로 처리되었습니다.");
+	}
 }
