be3a68962ae46c097c5bc62e8d92927943f9bcde
wjdwnsdnjs13 <67488973+wjdwnsdnjs13@users.noreply.github.com>
2024-05-01 14:43:22 +0900

    (feat) : LogFilter Post 통신 테스트를 위한 API 추가

--- src/main/java/team/gwon/haveameal/common/logging/LogFilterTestController.java
+++ src/main/java/team/gwon/haveameal/common/logging/LogFilterTestController.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.extern.slf4j.Slf4j;
+import team.gwon.haveameal.member.domain.MemberRegisterDto;
+
 @Slf4j
 @RestController
 @RequestMapping("test")
