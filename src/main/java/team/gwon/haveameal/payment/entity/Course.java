836ae276850082483175fe98aa54238137ef64ac
JaeUng <wodndrnjs@naver.com>
2024-05-02 13:59:38 +0900

    (refactor) : 매핑 오류에 따른 생성자 추가 - 롬복의 @Builder 적용 후 Mybatis 에서 객체로의 매핑과정에 문제가 발생함 - 기본 생성자가 존재하기 않아 생기는 문제로 확인 참고 #28

--- src/main/java/team/gwon/haveameal/payment/entity/Course.java
+++ src/main/java/team/gwon/haveameal/payment/entity/Course.java
@@ -1,10 +1,14 @@
 package team.gwon.haveameal.payment.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Builder
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Course {
 	int courseId;
 	String timing;
--- src/main/java/team/gwon/haveameal/payment/entity/PaymentWithCourseIncludeDetail.java
+++ src/main/java/team/gwon/haveameal/payment/entity/PaymentWithCourseIncludeDetail.java
@@ -1,10 +1,14 @@
 package team.gwon.haveameal.payment.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Builder
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class PaymentWithCourseIncludeDetail {
 	Payment payment;
 	PaymentDetail paymentDetail;
