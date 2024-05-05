59ef2fcb65c3907a79c4127d6eb286f5dd1cfe72
wjdwnsdnjs13 <67488973+wjdwnsdnjs13@users.noreply.github.com>
2024-05-05 12:24:33 +0900

    (refactor) : 중복되는 코드 제거

--- src/main/java/team/gwon/haveameal/common/config/SecurityConfig.java
+++ src/main/java/team/gwon/haveameal/common/config/SecurityConfig.java
@@ -15,8 +15,7 @@ public class SecurityConfig {
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		http
-			.csrf(AbstractHttpConfigurer::disable);
-		http
+			.csrf(AbstractHttpConfigurer::disable)
 			.authorizeHttpRequests(
 				authorize -> authorize
 					.requestMatchers("/test/*").permitAll()
