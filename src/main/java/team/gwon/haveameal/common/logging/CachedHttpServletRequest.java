ae0f14ba15ec8cf07d4698878fdf65622ee1884f
wjdwnsdnjs13 <67488973+wjdwnsdnjs13@users.noreply.github.com>
2024-05-05 13:49:14 +0900

    (feat) : HttpRequest를 감싼 래퍼 클레스 생성

---
+++ src/main/java/team/gwon/haveameal/common/logging/CachedHttpServletRequest.java
@@ -0,0 +1,7 @@
+package team.gwon.haveameal.common.logging;
+
+import jakarta.servlet.http.HttpServletRequestWrapper;
+
+public class CachedHttpServletRequest extends HttpServletRequestWrapper {
+
+}
