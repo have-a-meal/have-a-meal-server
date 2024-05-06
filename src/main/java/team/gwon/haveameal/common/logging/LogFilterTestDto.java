54e839ff40eeea71f907cf3bc9d316a166fdb712
wjdwnsdnjs13 <67488973+wjdwnsdnjs13@users.noreply.github.com>
2024-05-06 12:00:15 +0900

    (feat) : LogFIlter 테스트를 위한 DTO 생성

--- src/main/java/team/gwon/haveameal/common/logging/LogFilterTestDto.java
+++ src/main/java/team/gwon/haveameal/common/logging/LogFilterTestDto.java
@@ -1,4 +1,12 @@
 package team.gwon.haveameal.common.logging;
 
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@ToString
 public class LogFilterTestDto {
+	private String userId;
+	private String password;
+	private String name;
 }
