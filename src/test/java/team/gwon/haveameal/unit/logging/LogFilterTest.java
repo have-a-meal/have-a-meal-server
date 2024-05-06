a017e71c30dabe9eeda8548aa891026cf8d56554
wjdwnsdnjs13 <67488973+wjdwnsdnjs13@users.noreply.github.com>
2024-05-06 12:10:00 +0900

    (test) : LogFilter Get 테스트

--- src/test/java/team/gwon/haveameal/unit/logging/LogFilterTest.java
+++ src/test/java/team/gwon/haveameal/unit/logging/LogFilterTest.java
@@ -1,4 +1,18 @@
 package team.gwon.haveameal.unit.logging;
 
+
+import team.gwon.haveameal.common.logging.LogFilterTestDto;
+
+@SpringBootTest
+@AutoConfigureMockMvc
 public class LogFilterTest {
+	@Autowired
+	private MockMvc mockMvc;
+
+	@DisplayName("LogFilter Get 테스트")
+	@Test
+	public void testLogFilterGet() throws Exception {
+		mockMvc.perform(MockMvcRequestBuilders.get("/test/logTest"))
+			.andExpect(MockMvcResultMatchers.status().isOk());
+	}
 }
