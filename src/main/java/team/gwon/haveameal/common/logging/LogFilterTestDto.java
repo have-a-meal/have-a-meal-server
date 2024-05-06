1deaf02afcc03a48cf3b2a490f250258ae98cfce
wjdwnsdnjs13 <67488973+wjdwnsdnjs13@users.noreply.github.com>
2024-05-06 12:09:00 +0900

    (refactor) : Test를 위한 Builder 패턴 적용

--- src/main/java/team/gwon/haveameal/common/logging/LogFilterTestDto.java
+++ src/main/java/team/gwon/haveameal/common/logging/LogFilterTestDto.java
@@ -1,10 +1,12 @@
 package team.gwon.haveameal.common.logging;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.ToString;
 
 @Getter
 @ToString
+@Builder
 public class LogFilterTestDto {
 	private String userId;
 	private String password;
