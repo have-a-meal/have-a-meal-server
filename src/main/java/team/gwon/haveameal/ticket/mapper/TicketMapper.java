41faf243e53c0f0c976dcd9c65d94c39742173fa
JaeUng <wodndrnjs@naver.com>
2024-05-07 16:05:52 +0900

    (feat) : QrCode 사용 Mapper 메소드 및 쿼리 추가

--- src/main/java/team/gwon/haveameal/ticket/mapper/TicketMapper.java
+++ src/main/java/team/gwon/haveameal/ticket/mapper/TicketMapper.java
@@ -6,6 +6,7 @@ import org.apache.ibatis.annotations.Mapper;
 
 import team.gwon.haveameal.member.domain.MemberEntity;
 import team.gwon.haveameal.payment.entity.PaymentWithCourseIncludeDetail;
+import team.gwon.haveameal.ticket.domain.TicketEntity;
 
 @Mapper
 public interface TicketMapper {
@@ -13,4 +14,6 @@ public interface TicketMapper {
 	List<PaymentWithCourseIncludeDetail> findAllMyTickets(MemberEntity member);
 
 	Integer getQrCode(PaymentWithCourseIncludeDetail paymentWithCourseIncludeDetail);
+
+	Integer useQrCode(TicketEntity ticketEntity);
 }
