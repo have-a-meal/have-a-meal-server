df41590b3d633716d30c5c39bc32ea2c77d2c438
JaeUng <wodndrnjs@naver.com>
2024-05-07 16:06:14 +0900

    (test) : QrCode 사용 test 추가

--- src/test/java/team/gwon/haveameal/unit/ticket/TicketTest.java
+++ src/test/java/team/gwon/haveameal/unit/ticket/TicketTest.java
@@ -1,8 +1,11 @@
 package team.gwon.haveameal.unit.ticket;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.util.List;
 
 import javax.imageio.ImageIO;
 
@@ -16,18 +19,32 @@ import com.google.zxing.WriterException;
 import lombok.extern.slf4j.Slf4j;
 import team.gwon.haveameal.ticket.domain.QrCodeRequestDto;
 import team.gwon.haveameal.ticket.domain.QrCodeResponseDto;
+import team.gwon.haveameal.ticket.domain.QrCodeUseRequestDto;
+import team.gwon.haveameal.ticket.domain.QrCodeUseResponseDto;
+import team.gwon.haveameal.ticket.domain.TicketFindRequestDto;
+import team.gwon.haveameal.ticket.domain.TicketFindResponseDto;
 import team.gwon.haveameal.ticket.service.TicketService;
 
 @Slf4j
 @SpringBootTest
-class GetQrCodeTest {
+public class TicketTest {
 
 	@Autowired
-	private TicketService ticketService;
+	TicketService ticketService;
+
+	@DisplayName("티켓 조회 테스트")
+	@Test
+	void findTicket() {
+		// given
+		String memberId = "21860004";
+
+		List<TicketFindResponseDto> list = ticketService.findAllTickets(TicketFindRequestDto.from(memberId));
+		log.info("list : {}", list);
+	}
 
 	@DisplayName("QrCode 이미지 로드 테스트")
 	@Test
-	void findTicket() throws IOException, WriterException {
+	void getQrCode() throws IOException, WriterException {
 		// given
 		String memberId = "21860004";
 		String courseType = "A";
@@ -43,5 +60,20 @@ class GetQrCodeTest {
 		BufferedImage bufferedImage = ImageIO.read(inputStream);
 
 		// ImageIO.write(bufferedImage, "png", new File("./image.png"));
+	}
+
+	@DisplayName("QrCode 사용 시, 사용 변경 테스트")
+	@Test
+	void confirmQrCode() {
+		// given
+		Long ticketId = 1L;
+		Long mealId = 1L;
+
+		QrCodeUseRequestDto request = QrCodeUseRequestDto.of(ticketId, mealId);
+
+		QrCodeUseResponseDto response = ticketService.useQrCode(request);
+
+		assertEquals(response.getMessage(), "성공!");
+
 	}
 }
