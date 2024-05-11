ea2da02d4c2d03f821bca02137246c558d9a1c92
wonjun <zkdl2347@naver.com>
2024-05-11 21:04:01 +0900

    (test) : 네이버 오타변환 API 성능 업그레이드 #EAT-96 바로 사용할 수 있을 로직으로 변환 시킴. 재귀를 통해서 오타가 없을때까지 반복. 아예 다른 단어일때는 공백이 나오는 점은 피해갈 수 없음.

--- src/test/java/team/gwon/haveameal/unitexcel/NaverTypoCheckerTest.java
+++ src/test/java/team/gwon/haveameal/unitexcel/NaverTypoCheckerTest.java
@@ -21,29 +21,38 @@ public class NaverTypoCheckerTest {
 
 	@Test
 	public void test() {
+		String beforeWord = "마라상거";
+		log.info("success {} to {}", beforeWord, typoChecker(beforeWord));
+	}
+
+	private static String typoChecker(String word) {
 		String clientId = "YOUR_CLIENT_ID"; //애플리케이션 클라이언트 아이디
 		String clientSecret = "YOUR_CLIENT_SECRET"; //애플리케이션 클라이언트 시크릿
 
 		String text = null;
 		try {
-			text = URLEncoder.encode("닥고기장조림", "UTF-8");
+			text = URLEncoder.encode(word, "UTF-8");
 			//닥거기장조림을 예로 들어봤는데
 			//1차적으로 달고기장조림이 나옴 그 후 달고기장조림으로 돌리니까 닭고기 장조림이 나옴.
 			//검증로직을 반복함으로써 정확성을 높이는 방향도 괜찮다고 생각됨.
 			//옳은 결과일때는 공란을 반환하니까 공란을 반환할때까지 한다는게 좋을듯
 			//아예 이상한 말이 나오면 공란을 반환해서 이 부분은 한계라고 생각이 듬.ex)기무치=>공란 나옴.
 			//재귀를 사용해서 errata값이 공란일때 해당 text return 해주고 아니면 계속 반복.
+			//ex)마라상거->마라샹거->마라샹궈
 		} catch (UnsupportedEncodingException e) {
 			throw new RuntimeException("검색어 인코딩 실패", e);
 		}
 
-		String apiUrl = "https://openapi.naver.com/v1/search/errata.json?query=" + text;
+		String apiURL = "https://openapi.naver.com/v1/search/errata.json?query=" + text;
 
 		Map<String, String> requestHeaders = new HashMap<>();
 		requestHeaders.put("X-Naver-Client-Id", clientId);
 		requestHeaders.put("X-Naver-Client-Secret", clientSecret);
-		String responseBody = get(apiUrl, requestHeaders);
-		log.info(responseBody);
+		String responseBody = get(apiURL, requestHeaders);
+		if (!responseBody.split(":")[1].replace("}", "").equals("\"\"")) {
+			word = typoChecker(responseBody.split(":")[1].replaceAll("[\"}]", ""));
+		}
+		return word;
 	}
 
 	private static String get(String apiUrl, Map<String, String> requestHeaders) {
