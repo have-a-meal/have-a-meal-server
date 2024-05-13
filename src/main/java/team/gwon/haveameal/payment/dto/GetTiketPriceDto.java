ab2188790a48866629bc52d56bf6cd04c5bf93b3
wjdwnsdnjs13 <67488973+wjdwnsdnjs13@users.noreply.github.com>
2024-05-13 14:43:59 +0900

    (feat) : 티켓 가격 가져오는 DTO 추가

--- src/main/java/team/gwon/haveameal/payment/dto/GetTiketPriceDto.java
+++ src/main/java/team/gwon/haveameal/payment/dto/GetTiketPriceDto.java
@@ -1,4 +1,12 @@
 package team.gwon.haveameal.payment.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public class GetTiketPriceDto {
+	String timing;
+	String courseType;
+	String memberType;
 }
