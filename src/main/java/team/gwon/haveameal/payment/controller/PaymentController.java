2555a0fbd2c7c4bd2b6249927678b95fecb96c97
wjdwnsdnjs13 <67488973+wjdwnsdnjs13@users.noreply.github.com>
2024-05-13 14:43:01 +0900

    (feat) : 티켓 가격 가져오는 API 추가

--- src/main/java/team/gwon/haveameal/payment/controller/PaymentController.java
+++ src/main/java/team/gwon/haveameal/payment/controller/PaymentController.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,6 +12,7 @@ import com.siot.IamportRestClient.exception.IamportResponseException;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import team.gwon.haveameal.payment.dto.GetTiketPriceDto;
 import team.gwon.haveameal.payment.service.PaymentService;
 
 @Slf4j
@@ -25,6 +27,13 @@ public class PaymentController {
 	public void paymentByImpUid(@PathVariable("impUid") String impUid) throws IamportResponseException, IOException {
 	public void verifyPayment(@PathVariable("impUid") String impUid) throws IamportResponseException, IOException {
 		paymentService.verifyPayment(impUid);
+	}
+
+	@GetMapping("/getTiketPrice")
+	public void getTiketPrice(@PathVariable("timing") String timing, @PathVariable("courseType") String courseType,
+		@PathVariable("memberType") String memberType) {
+		GetTiketPriceDto getTiketPriceDto = new GetTiketPriceDto(timing, courseType, memberType);
+		paymentService.getTiketPrice(getTiketPriceDto);
 	}
 
 	@GetMapping("/test")
