4cd3f339570cf57a3d3d55493f0dd30f35d033b0
wjdwnsdnjs13 <67488973+wjdwnsdnjs13@users.noreply.github.com>
2024-05-13 14:45:54 +0900

    (feat) : 티켓 가격 가져오기 기능 추가

--- src/main/java/team/gwon/haveameal/payment/mapper/PaymentMapper.java
+++ src/main/java/team/gwon/haveameal/payment/mapper/PaymentMapper.java
@@ -1,6 +1,10 @@
 package team.gwon.haveameal.payment.mapper;
 
+import team.gwon.haveameal.payment.dto.GetTiketPriceDto;
+
 public interface PaymentMapper {
+
+	int getTiketPrice(GetTiketPriceDto getTiketPriceDto);
 
 	int amount(String impUid);
 
