977955c53f4a654c5449e906f5d1a77c58ee589c
wjdwnsdnjs13 <67488973+wjdwnsdnjs13@users.noreply.github.com>
2024-05-13 14:49:20 +0900

    (feat) : 티켓 가격 가져오는 기능 추가

--- src/main/java/team/gwon/haveameal/payment/service/PaymentService.java
+++ src/main/java/team/gwon/haveameal/payment/service/PaymentService.java
@@ -4,11 +4,15 @@ import java.io.IOException;
 
 import com.siot.IamportRestClient.exception.IamportResponseException;
 
+import team.gwon.haveameal.payment.dto.GetTiketPriceDto;
+
 public interface PaymentService {
 
 	void init();
 
 	void test();
+
+	void getTiketPrice(GetTiketPriceDto getTiketPriceDto);
 
 	void verifyPayment(String impUid) throws IamportResponseException, IOException;
 
