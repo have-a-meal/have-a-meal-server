e7f3c7d83db3eff58b4f70e91e8ffe83e75e89b1
wonjun <zkdl2347@naver.com>
2024-05-13 17:32:09 +0900

    (feat) : 네이버 오타변환 응답용 객체 #EAT-96

---
+++ src/main/java/team/gwon/haveameal/excelextract/dto/ValidFood.java
@@ -0,0 +1,16 @@
+package team.gwon.haveameal.excelextract.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class ValidFood {
+	private String errata;
+}
