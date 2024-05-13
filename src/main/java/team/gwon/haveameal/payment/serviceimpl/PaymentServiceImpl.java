9fd6dfe7c75deae657345b479fb2bb5ea920f8bc
wjdwnsdnjs13 <67488973+wjdwnsdnjs13@users.noreply.github.com>
2024-05-13 14:47:22 +0900

    (refactor) : 결제 검증 확인 수정

--- src/main/java/team/gwon/haveameal/payment/serviceimpl/PaymentServiceImpl.java
+++ src/main/java/team/gwon/haveameal/payment/serviceimpl/PaymentServiceImpl.java
@@ -52,7 +52,8 @@ public class PaymentServiceImpl implements PaymentService {
 	public void verifyPayment(String impUid) throws IamportResponseException, IOException {
 		log.info("portOne api 연동 확인 : " + iamportClient);
 		IamportResponse<Payment> iamportResponse = iamportClient.paymentByImpUid(impUid);
-		Long amount = iamportResponse.getResponse().getAmount().longValue(); // 결제 금액
+		int amount = paymentMapper.amount(impUid);
+		Long amountIamport = iamportResponse.getResponse().getAmount().longValue(); // 결제 금액
 		String name = iamportResponse.getResponse().getName(); // 상품명
 		String status = iamportResponse.getResponse().getStatus(); //결제 상태
 		log.info("데이터 확인 : , amount = {}, name = {}, status = {}", amount, name, status);
