7cd6544eb587e23c5d412e039f80edc46dc9d750
wonjun <zkdl2347@naver.com>
2024-05-16 16:17:03 +0900

    (feat) : 엑셀 upload 호출 수정 및 update 호출 구현 #EAT-96

--- src/main/java/team/gwon/haveameal/excelextract/controller/ExcelExtractController.java
+++ src/main/java/team/gwon/haveameal/excelextract/controller/ExcelExtractController.java
@@ -1,6 +1,10 @@
 package team.gwon.haveameal.excelextract.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
@@ -10,14 +14,23 @@ import team.gwon.haveameal.excelextract.component.MenuFacade;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/manager")
 public class ExcelExtractController {
 	/*
 	 마지막 통신 부분에서 구현을 하기 위해 남겨둔 미완성 코드.
 	*/
 	private final MenuFacade menuFacade;
 
-	@PostMapping("/upload")
-	public void excelRead(@RequestPart(value = "file", required = false) MultipartFile multipartFile) throws Exception {
+	@PostMapping("/excel")
+	public ResponseEntity<String> excelRead(@RequestPart(value = "file") MultipartFile multipartFile) throws Exception {
 		menuFacade.uploadExcel(multipartFile);
+		return ResponseEntity.status(HttpStatus.OK).body("success");
+	}
+
+	@PutMapping("/excel")
+	public ResponseEntity<String> excelUpdate(@RequestPart(value = "file") MultipartFile multipartFile) throws
+		Exception {
+		menuFacade.updateAfterDeleteExcel(multipartFile);
+		return ResponseEntity.status(HttpStatus.OK).body("success");
 	}
 }
