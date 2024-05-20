c0b04d75597d814d18546f0d57e31ddfd3696d2e
wjdwnsdnjs13 <67488973+wjdwnsdnjs13@users.noreply.github.com>
2024-05-20 17:44:02 +0900

    (feat) : DTO to Entity 구현

--- src/main/java/team/gwon/haveameal/payment/dto/TicketPriceRequestDto.java
+++ src/main/java/team/gwon/haveameal/payment/dto/TicketPriceRequestDto.java
@@ -3,6 +3,9 @@ package team.gwon.haveameal.payment.dto;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.ToString;
+import team.gwon.haveameal.payment.entity.Course;
+import team.gwon.haveameal.payment.entity.CourseDetail;
+import team.gwon.haveameal.payment.entity.CourseWithDetail;
 
 @Getter
 @AllArgsConstructor
@@ -12,4 +15,13 @@ public class TicketPriceRequestDto {
 	private String courseType;
 	private String memberType;
 
+	public CourseWithDetail toCourseWithDetail() {
+		Course course = Course.builder()
+			.timing(this.timing)
+			.courseType(this.courseType).build();
+		CourseDetail courseDetail = CourseDetail.builder()
+			.memberType(this.memberType).build();
+		return CourseWithDetail.builder().course(course).courseDetail(courseDetail).build();
+
+	}
 }
