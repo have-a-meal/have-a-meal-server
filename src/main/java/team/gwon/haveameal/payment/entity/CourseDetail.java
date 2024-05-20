8aa3877fa9a8e77fe42415a61fb0b73b3ddbafcb
wjdwnsdnjs13 <67488973+wjdwnsdnjs13@users.noreply.github.com>
2024-05-20 17:41:05 +0900

    (feat) : 빌더 패턴 적용

--- src/main/java/team/gwon/haveameal/payment/entity/CourseDetail.java
+++ src/main/java/team/gwon/haveameal/payment/entity/CourseDetail.java
@@ -1,8 +1,10 @@
 package team.gwon.haveameal.payment.entity;
 
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
+@Builder
 public class CourseDetail {
 	int courseDetailId;
 	String memberType;
--- src/main/java/team/gwon/haveameal/payment/entity/CourseWithDetail.java
+++ src/main/java/team/gwon/haveameal/payment/entity/CourseWithDetail.java
@@ -1,8 +1,10 @@
 package team.gwon.haveameal.payment.entity;
 
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
+@Builder
 public class CourseWithDetail {
 	Course course;
 	CourseDetail courseDetail;
