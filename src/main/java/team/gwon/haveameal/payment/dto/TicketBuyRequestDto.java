71d6ca381ec3ef33703c9a143873c8a63c8d1f6a
wjdwnsdnjs13 <67488973+wjdwnsdnjs13@users.noreply.github.com>
2024-05-22 12:04:57 +0900

    (feat) : 티켓 구매 Request Dto 구현

--- src/main/java/team/gwon/haveameal/payment/dto/TicketBuyRequestDto.java
+++ src/main/java/team/gwon/haveameal/payment/dto/TicketBuyRequestDto.java
@@ -1,4 +1,28 @@
 package team.gwon.haveameal.payment.dto;
 
+import java.time.LocalDateTime;
+
+import lombok.Getter;
+import lombok.ToString;
+import team.gwon.haveameal.common.util.UuidProvider;
+import team.gwon.haveameal.payment.entity.Payment;
+
+@Getter
+@ToString
 public class TicketBuyRequestDto {
+	private String memberId;
+	private int courseId;
+	private String pgProvider; //"kakaopay" or "tosspay"
+	private String payMethod; //"kakaopay" or "tosspay"
+
+	public Payment toPayment() {
+		byte[] paymentId = UuidProvider.stringToByte(UuidProvider.generateSequentialUuid());
+		return Payment.builder()
+			.paymentId(paymentId)
+			.memberId(this.memberId)
+			.courseId(this.courseId)
+			.pgProvider(this.pgProvider)
+			.payMethod(this.payMethod)
+			.requestAt(LocalDateTime.now()).build();
+	}
 }
