221ea3c6e8256f62cffd7ffc34b785a2e114bad7
wjdwnsdnjs13 <67488973+wjdwnsdnjs13@users.noreply.github.com>
2024-05-22 12:13:17 +0900

    (feat) : 구매 이력 생성 구현

--- src/main/java/team/gwon/haveameal/payment/controller/PaymentController.java
+++ src/main/java/team/gwon/haveameal/payment/controller/PaymentController.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,6 +13,9 @@ import com.siot.IamportRestClient.exception.IamportResponseException;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import team.gwon.haveameal.payment.dto.PaymentVerifyRequestDto;
+import team.gwon.haveameal.payment.dto.TicketBuyRequestDto;
+import team.gwon.haveameal.payment.dto.TicketBuyResponseDto;
 import team.gwon.haveameal.payment.dto.TicketPriceRequestDto;
 import team.gwon.haveameal.payment.dto.TicketPriceResponseDto;
 import team.gwon.haveameal.payment.service.PaymentService;
@@ -35,9 +39,12 @@ public class PaymentController {
 		TicketPriceResponseDto ticketPriceResponseDto = paymentService.getTicketPrice(getTiketPriceDto);
 	}
 
-	@PostMapping("/buy")
-	public void buyTiket() {
-		paymentService.createPayment();
+	@PostMapping("")
+	public void buyTiket(@RequestBody TicketBuyRequestDto ticketBuyRequestDto) {
+		log.info("ticketBuyRequestDto : {}", ticketBuyRequestDto);
+
+		TicketBuyResponseDto ticketBuyResponseDto = paymentService.createPayment(ticketBuyRequestDto);
+		log.info("TicketBuyResponseDto : {}", ticketBuyResponseDto);
 	}
 
 	@GetMapping("/test")
