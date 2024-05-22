a5b6652cc351394d5737799f56239ac46b725c16
wonjun <zkdl2347@naver.com>
2024-05-22 12:01:33 +0900

    (comment) : 안쓰는 코드 주석 제거 #EAT-96

--- src/main/java/team/gwon/haveameal/excelextract/error/ErrorCode.java
+++ src/main/java/team/gwon/haveameal/excelextract/error/ErrorCode.java
@@ -12,10 +12,6 @@ public enum ErrorCode {
 	SUCCESS(HttpStatus.OK, "OK"),
 
 	//== 400 ==//
-	// NOT_SUPPORTED_HTTP_METHOD(HttpStatus.BAD_REQUEST, "지원하지 않는 Http Method 방식입니다."),
-	// NOT_VALID_METHOD_ARGUMENT(HttpStatus.BAD_REQUEST, "유효하지 않은 Request Body 혹은 Argument입니다."),
-	// USER_NOT_FOUND(HttpStatus.BAD_REQUEST, "해당 사용자를 찾을 수 없습니다."),
-	// ITEM_NOT_FOUND(HttpStatus.BAD_REQUEST, "해당 상품을 찾을 수 없습니다.");
 	INCORRECT_DATE_FORMAT(HttpStatus.BAD_REQUEST, "날짜 형식 맞지 않습니다."),
 	INCORRECT_COURSE_ARGUMENT(HttpStatus.BAD_REQUEST, "존재하지 않는 코스 입니다."),
 	NOT_VALID_EXCEL_DATA(HttpStatus.BAD_REQUEST, "제공된 템플릿 형식에 맞게 다시 작성 부탁 드립니다."),
