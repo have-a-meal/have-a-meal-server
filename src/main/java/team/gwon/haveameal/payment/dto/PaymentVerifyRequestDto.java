7169f2195b9e167c001f840346bcd561403ab409
wjdwnsdnjs13 <67488973+wjdwnsdnjs13@users.noreply.github.com>
2024-05-22 12:03:55 +0900

    (feat) : 티켓 구매 Request Dto 구현

--- src/main/java/team/gwon/haveameal/payment/dto/PaymentVerifyRequestDto.java
+++ src/main/java/team/gwon/haveameal/payment/dto/PaymentVerifyRequestDto.java
@@ -1,4 +1,10 @@
 package team.gwon.haveameal.payment.dto;
 
+import lombok.Getter;
+
+@Getter
 public class PaymentVerifyRequestDto {
+	private byte[] payment_id;
+	private String impUid;
+	// private PortOnePaymentStatus message;
 }
