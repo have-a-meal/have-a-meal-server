29dd8299f971f9bda1cd0fabe588894102ae6962
wjdwnsdnjs13 <67488973+wjdwnsdnjs13@users.noreply.github.com>
2024-05-31 22:59:18 +0900

    (feat) : 결제 내역 조회 구현

--- src/main/java/team/gwon/haveameal/payment/mapper/PaymentMapper.java
+++ src/main/java/team/gwon/haveameal/payment/mapper/PaymentMapper.java
@@ -1,10 +1,14 @@
 package team.gwon.haveameal.payment.mapper;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
 
 import team.gwon.haveameal.payment.entity.CourseWithDetail;
 import team.gwon.haveameal.payment.entity.Payment;
 import team.gwon.haveameal.payment.entity.PaymentDetail;
+import team.gwon.haveameal.payment.entity.PaymentWithCourseIncludeDetail;
+import team.gwon.haveameal.payment.entity.PaymentWithDetail;
 import team.gwon.haveameal.payment.entity.TicketPrice;
 
 @Mapper
@@ -17,4 +21,10 @@ public interface PaymentMapper {
 	int createPayment(Payment payment);
 
 	int createPaymentDetail(PaymentDetail paymentDetail);
+
+	List<PaymentWithDetail> getPaymentWithDetail(String memberId);
+
+	List<CourseWithDetail> getCourseWithDetail();
+
+	PaymentWithCourseIncludeDetail getPaymentTransaction(String memberId);
 }
