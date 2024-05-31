5d35b6654be0943975c5f4bddd6ce083901edfc9
geonwoo0423 <hyunsung0423@naver.com>
2024-05-31 09:19:04 +0900

    (rename) : 파일명 수정 #EAT-118

--- src/main/java/team/gwon/haveameal/member/registrationservice/personaldataencryption/SecureConfig.java
+++ src/main/java/team/gwon/haveameal/member/registrationservice/personaldataencryption/SecureConfig.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class SecurityConfig {
+public class SecureConfig {
 	@Bean
 	public SecureRandom secureRandom() {
 		return new SecureRandom();
