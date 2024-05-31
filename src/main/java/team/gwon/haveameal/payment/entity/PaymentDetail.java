739f70ab5fbbbf01d54179971a84c9c90ab150e9
wjdwnsdnjs13 <67488973+wjdwnsdnjs13@users.noreply.github.com>
2024-05-31 22:56:45 +0900

    (feat) : MyBatis 매핑 에러 방지를 위한 생성자 추가

--- src/main/java/team/gwon/haveameal/payment/entity/PaymentDetail.java
+++ src/main/java/team/gwon/haveameal/payment/entity/PaymentDetail.java
@@ -2,13 +2,17 @@ package team.gwon.haveameal.payment.entity;
 
 import java.time.LocalDateTime;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 @Getter
 @Builder
 @ToString
+@AllArgsConstructor
+@NoArgsConstructor
 public class PaymentDetail {
 	byte[] paymentDetailId;
 	int refundReasonId;
