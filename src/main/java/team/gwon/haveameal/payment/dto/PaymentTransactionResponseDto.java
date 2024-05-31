cded861c246c0122e9257a04fe18f7edacf7cf6d
wjdwnsdnjs13 <67488973+wjdwnsdnjs13@users.noreply.github.com>
2024-05-31 22:58:35 +0900

    (feat) : 결제 내역 조회 응답 DTO 구현

--- src/main/java/team/gwon/haveameal/payment/dto/PaymentTransactionResponseDto.java
+++ src/main/java/team/gwon/haveameal/payment/dto/PaymentTransactionResponseDto.java
@@ -1,4 +1,19 @@
 package team.gwon.haveameal.payment.dto;
 
+import java.time.LocalDateTime;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
 public class PaymentTransactionResponseDto {
+	private byte[] paymentId;
+	private int courseId;
+	private String status;
+	private LocalDateTime accountDate;
+	private LocalDateTime refundDate;
+	private int price;
 }
