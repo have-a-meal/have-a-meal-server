5ecc584c72a50f85ea851668782b6aaddd3cbd21
geonwoo0423 <hyunsung0423@naver.com>
2024-05-31 09:17:58 +0900

    (comment) : 주석 수정 #EAT-118

--- src/main/java/team/gwon/haveameal/member/util/IdGenerator.java
+++ src/main/java/team/gwon/haveameal/member/util/IdGenerator.java
@@ -9,7 +9,7 @@ public class IdGenerator {
 	private static final Random random = new Random();
 
 	/*
-	DB에 저장된 값과 비교 필요.
+	DB에 저장된 값과 중복 비교 필요.
 	*/
 	public static String generateId() {
 		int id = random.nextInt(90000000) + 10000000;
