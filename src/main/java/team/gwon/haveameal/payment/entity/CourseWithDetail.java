2935f3eec433ef95b735d970fc7f04e54c68dc65
wjdwnsdnjs13 <67488973+wjdwnsdnjs13@users.noreply.github.com>
2024-05-31 22:56:39 +0900

    (feat) : MyBatis 매핑 에러 방지를 위한 생성자 추가

--- src/main/java/team/gwon/haveameal/payment/entity/CourseWithDetail.java
+++ src/main/java/team/gwon/haveameal/payment/entity/CourseWithDetail.java
@@ -1,10 +1,14 @@
 package team.gwon.haveameal.payment.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class CourseWithDetail {
 	Course course;
 	CourseDetail courseDetail;
