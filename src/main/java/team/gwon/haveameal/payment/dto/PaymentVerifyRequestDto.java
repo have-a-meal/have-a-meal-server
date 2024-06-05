681c4f7af2307187d643e9e0a9901c01161f787c
wjdwnsdnjs13 <67488973+wjdwnsdnjs13@users.noreply.github.com>
2024-06-05 21:43:26 +0900

    (feat) : IamportResponse를 받아서 PaymentDetail Entity로 변환 구현

--- src/main/java/team/gwon/haveameal/payment/dto/PaymentVerifyRequestDto.java
+++ src/main/java/team/gwon/haveameal/payment/dto/PaymentVerifyRequestDto.java
@@ -1,10 +1,34 @@
 package team.gwon.haveameal.payment.dto;
 
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Date;
+
+import com.siot.IamportRestClient.response.IamportResponse;
+
 import lombok.Getter;
+import team.gwon.haveameal.payment.entity.PaymentDetail;
 
 @Getter
 public class PaymentVerifyRequestDto {
 	private byte[] paymentId;
 	private String impUid;
+
 	// private PortOnePaymentStatus message;
+
+	public PaymentDetail toPaymentDetail(IamportResponse<com.siot.IamportRestClient.response.Payment> iamportResponse) {
+		return PaymentDetail.builder()
+			.paymentDetailId(this.paymentId)
+			.status(iamportResponse.getResponse().getStatus())
+			.impUid(this.impUid)
+			.pgTid(iamportResponse.getResponse().getPgTid())
+			.paidAt(toLocalDateTime(iamportResponse.getResponse().getPaidAt())).build();
+	}
+
+	public LocalDateTime toLocalDateTime(Date date) {
+		return Instant.ofEpochMilli(date.getTime())
+			.atZone(ZoneId.systemDefault())
+			.toLocalDateTime();
+	}
 }
