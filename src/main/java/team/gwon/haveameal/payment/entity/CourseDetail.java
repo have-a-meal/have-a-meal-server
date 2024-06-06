8793f6c4a687fd3c3707d794761ff669bc376781
wjdwnsdnjs13 <67488973+wjdwnsdnjs13@users.noreply.github.com>
2024-06-06 21:21:44 +0900

    (feat) : ToString 추가

--- src/main/java/team/gwon/haveameal/payment/entity/CourseDetail.java
+++ src/main/java/team/gwon/haveameal/payment/entity/CourseDetail.java
@@ -4,9 +4,11 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Getter
 @Builder
+@ToString
 @NoArgsConstructor
 @AllArgsConstructor
 public class CourseDetail {
