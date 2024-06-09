522eb9501789070238a8609503cee01c6e803781
wonjun <zkdl2347@naver.com>
2024-06-09 16:55:21 +0900

    (test) : 네이버 맞춤법 api test #EAT-96

--- src/test/java/team/gwon/haveameal/unitexcel/NaverTypoCheckerTest.java
+++ src/test/java/team/gwon/haveameal/unitexcel/NaverTypoCheckerTest.java
@@ -15,34 +15,36 @@ import java.util.Map;
 import org.junit.jupiter.api.Test;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 import lombok.extern.slf4j.Slf4j;
-import team.gwon.haveameal.excelextract.dto.ValidFood;
 
 @Slf4j
 public class NaverTypoCheckerTest {
 
 	@Test
-	public void test() throws JsonProcessingException {
-		String beforeWord = "마라상거";
-		log.info("success {} to {}", beforeWord, typoChecker(beforeWord));
+	public void test() throws JsonProcessingException, UnsupportedEncodingException {
+		String beforeWord = "닥고기계란장조림";
+		String beforeWord2 = "배추기치";
+		// String text1 = URLEncoder.encode(beforeWord, "UTF-8");
+		// String text2 = URLEncoder.encode(beforeWord2, "UTF-8");
+		log.info("success {} to {}", beforeWord, typoChecker(beforeWord, beforeWord2));
 	}
 
-	private static String typoChecker(String word) throws JsonProcessingException {
+	private static String typoChecker(String word, String word2) throws JsonProcessingException {
 		String clientId = "DEHgldW1cD3gEFXGnjiy"; //애플리케이션 클라이언트 아이디
 		String clientSecret = "SpDNui6VfJ"; //애플리케이션 클라이언트 시크릿
 
 		String text = null;
+		//닥거기장조림을 예로 들어봤는데
+		//1차적으로 달고기장조림이 나옴 그 후 달고기장조림으로 돌리니까 닭고기 장조림이 나옴.
+		//검증로직을 반복함으로써 정확성을 높이는 방향도 괜찮다고 생각됨.
+		//옳은 결과일때는 공란을 반환하니까 공란을 반환할때까지 한다는게 좋을듯
+		//아예 이상한 말이 나오면 공란을 반환해서 이 부분은 한계라고 생각이 듬.ex)기무치=>공란 나옴.
+		//재귀를 사용해서 errata값이 공란일때 해당 text return 해주고 아니면 계속 반복.
+		//ex)마라상거->마라샹거->마라샹궈
 		try {
-			text = URLEncoder.encode(word, "UTF-8");
-			//닥거기장조림을 예로 들어봤는데
-			//1차적으로 달고기장조림이 나옴 그 후 달고기장조림으로 돌리니까 닭고기 장조림이 나옴.
-			//검증로직을 반복함으로써 정확성을 높이는 방향도 괜찮다고 생각됨.
-			//옳은 결과일때는 공란을 반환하니까 공란을 반환할때까지 한다는게 좋을듯
-			//아예 이상한 말이 나오면 공란을 반환해서 이 부분은 한계라고 생각이 듬.ex)기무치=>공란 나옴.
-			//재귀를 사용해서 errata값이 공란일때 해당 text return 해주고 아니면 계속 반복.
-			//ex)마라상거->마라샹거->마라샹궈
+			text = URLEncoder.encode(word, "UTF-8") + "," + URLEncoder.encode(word2, "UTF-8");
+
 		} catch (UnsupportedEncodingException e) {
 			throw new RuntimeException("검색어 인코딩 실패", e);
 		}
@@ -53,12 +55,12 @@ public class NaverTypoCheckerTest {
 		requestHeaders.put("X-Naver-Client-Id", clientId);
 		requestHeaders.put("X-Naver-Client-Secret", clientSecret);
 		String responseBody = get(apiUrl, requestHeaders);
-		ObjectMapper objectMapper = new ObjectMapper();
-		ValidFood validFood = objectMapper.readValue(responseBody, ValidFood.class);
-		if (!validFood.getErrata().isEmpty()) {
-			word = typoChecker(validFood.getErrata());
-		}
-		return word;
+		// ObjectMapper objectMapper = new ObjectMapper();
+		// ValidFood validFood = objectMapper.readValue(responseBody, ValidFood.class);
+		// if (!validFood.getErrata().isEmpty()) {
+		// 	word = typoChecker(validFood.getErrata());
+		// }
+		return responseBody;
 	}
 
 	private static String get(String apiUrl, Map<String, String> requestHeaders) {
