e142894eb1658440396f1ae16cccdea617c32698
JaeUng <wodndrnjs@naver.com>
2024-06-12 15:34:17 +0900

    (feat) : RedisHash 추가

--- src/main/java/team/gwon/haveameal/common/domain/Token.java
+++ src/main/java/team/gwon/haveameal/common/domain/Token.java
@@ -1,10 +1,13 @@
 package team.gwon.haveameal.common.domain;
 
+import org.springframework.data.redis.core.RedisHash;
+
 import lombok.Builder;
 import lombok.Getter;
 
 @Builder
 @Getter
+@RedisHash("token")
 public class Token {
 	private String tokenId;
 	private String accessToken;
