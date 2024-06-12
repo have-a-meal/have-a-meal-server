7119d7aace73fc90ae08445f07ddda3bb456c5ef
JaeUng <wodndrnjs@naver.com>
2024-06-12 15:37:06 +0900

    (feat) : SecurityConfig 설정 추가

--- src/main/java/team/gwon/haveameal/common/config/SecurityConfig.java
+++ src/main/java/team/gwon/haveameal/common/config/SecurityConfig.java
@@ -16,6 +16,8 @@ public class SecurityConfig {
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		http
 			.csrf(AbstractHttpConfigurer::disable)
+			.formLogin(AbstractHttpConfigurer::disable)
+			.httpBasic(AbstractHttpConfigurer::disable)
 			.authorizeHttpRequests(
 				authorize -> authorize
 					.requestMatchers("/*", "/*/*").permitAll()
