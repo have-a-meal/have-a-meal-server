af88ced32c903fdfba6ef413079befe4e96083ca
wjdwnsdnjs13 <67488973+wjdwnsdnjs13@users.noreply.github.com>
2024-06-14 01:31:14 +0900

    (refactor) : API 리턴 타입 수정

--- src/main/java/team/gwon/haveameal/payment/controller/PaymentController.java
+++ src/main/java/team/gwon/haveameal/payment/controller/PaymentController.java
@@ -3,6 +3,8 @@ package team.gwon.haveameal.payment.controller;
 import java.io.IOException;
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -32,36 +34,40 @@ public class PaymentController {
 	private final PaymentService paymentService;
 
 	@GetMapping("/verify")
-	public void verifyPayment(@Valid PaymentVerifyRequestDto paymentVerifyRequestDto) throws
+	public ResponseEntity<?> verifyPayment(@Valid PaymentVerifyRequestDto paymentVerifyRequestDto) throws
 		IamportResponseException,
 		IOException {
 		log.info("paymentVerifyRequestDto : {}", paymentVerifyRequestDto);
-		paymentService.verifyPayment(paymentVerifyRequestDto);
+		boolean flag = paymentService.verifyPayment(paymentVerifyRequestDto);
+		if (flag) {
+			return ResponseEntity.status(HttpStatus.OK).build();
+		}
+		return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
 	}
 
 	@GetMapping("/tiketPrice")
-	public void getTiketPrice(@Valid TicketPriceRequestDto ticketPriceRequestDto) {
+	public ResponseEntity<TicketPriceResponseDto> getTiketPrice(@Valid TicketPriceRequestDto ticketPriceRequestDto) {
 		log.info("ticketPriceRequestDto DTO : {}", ticketPriceRequestDto);
 		TicketPriceResponseDto ticketPriceResponseDto = paymentService.getTicketPrice(ticketPriceRequestDto);
+		return ResponseEntity.status(HttpStatus.OK).body(ticketPriceResponseDto);
 	}
 
 	@PostMapping("")
-	public void buyTiket(@RequestBody @Valid TicketBuyRequestDto ticketBuyRequestDto) {
+	public ResponseEntity<TicketBuyResponseDto> buyTiket(@RequestBody @Valid TicketBuyRequestDto ticketBuyRequestDto) {
 		log.info("ticketBuyRequestDto : {}", ticketBuyRequestDto);
 
 		TicketBuyResponseDto ticketBuyResponseDto = paymentService.createPayment(ticketBuyRequestDto);
 		log.info("TicketBuyResponseDto : {}", ticketBuyResponseDto);
-	}
 
-	@GetMapping("/test")
-	public void test() {
-		paymentService.test();
+		return ResponseEntity.status(HttpStatus.CREATED).body(ticketBuyResponseDto);
 	}
 
 	@GetMapping("/transaction/{memberId}")
-	public void getPaymentTransactionList(@PathVariable String memberId) {
+	public ResponseEntity<List<PaymentTransactionResponseDto>> getPaymentTransactionList(@PathVariable String memberId) {
 		List<PaymentTransactionResponseDto> paymentTransactionResponseDtoList = paymentService.getPaymentTransaction(
 			memberId);
 		log.info("결제 목록 : {}", paymentTransactionResponseDtoList.toString());
+
+		return ResponseEntity.status(HttpStatus.OK).body(paymentTransactionResponseDtoList);
 	}
 }
