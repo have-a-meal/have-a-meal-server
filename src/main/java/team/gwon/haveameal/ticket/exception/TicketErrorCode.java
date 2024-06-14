413f3afc2c6252e87ad67de7d233626caa1f73a9
JaeUng <wodndrnjs@naver.com>
2024-06-14 14:47:05 +0900

    (feat) : ticketErrorCode 추가

--- src/main/java/team/gwon/haveameal/ticket/exception/TicketErrorCode.java
+++ src/main/java/team/gwon/haveameal/ticket/exception/TicketErrorCode.java
@@ -8,7 +8,8 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum TicketErrorCode {
-	TICKET_NOT_FOUND(HttpStatus.BAD_REQUEST, "검색 결과가 없습니다.");
+	TICKET_NOT_FOUND(HttpStatus.BAD_REQUEST, "검색 결과가 없습니다."),
+	TICKET_SQL_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "데이터베이스 에러입니다.");
 
 	private final HttpStatus status;
 	private final String message;
