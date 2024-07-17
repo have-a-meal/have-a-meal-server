0adf7685d6352045a75cc88ce16ee9a4b4b00499
wonjun <zkdl2347@naver.com>
2024-07-17 22:43:11 +0900

    (feat) : 필터 등록 및 권한 허융 uri 설정 #EAT-141

--- src/main/java/team/gwon/haveameal/common/config/SecurityConfig.java
+++ src/main/java/team/gwon/haveameal/common/config/SecurityConfig.java
@@ -4,13 +4,31 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import lombok.RequiredArgsConstructor;
+import team.gwon.haveameal.common.util.TokenAuthenticationFilter;
+import team.gwon.haveameal.common.util.TokenProvider;
 
 @Configuration
 @RequiredArgsConstructor
 public class SecurityConfig {
+	private final TokenProvider tokenProvider;
+
+	// private TokenAuthenticationFilter tokenAuthenticationFilter(TokenProvider tokenProvider) {
+	// 	return new TokenAuthenticationFilter(tokenProvider);
+	// }
+	//
+	// private Filter tokenExceptionFilter() {
+	// 	return new TokenExceptionFilter();
+	// }
+	// @Bean
+	// public WebSecurityCustomizer webSecurityCustomizer() {
+	// 	return (web) -> web.ignoring().requestMatchers(new AntPathRequestMatcher("/members/email"));
+	// }
+	// private final TokenExceptionFilter tokenExceptionFilter;
 
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -18,8 +36,13 @@ public class SecurityConfig {
 			.csrf(AbstractHttpConfigurer::disable)
 			.authorizeHttpRequests(
 				authorize -> authorize
-					.requestMatchers("/*", "/*/*").permitAll()
-			);
+					.requestMatchers("/members/**", "/error").permitAll()
+					// .anyRequest().hasRole("USER")
+					.anyRequest().authenticated()
+			).addFilterBefore(new TokenAuthenticationFilter(tokenProvider), UsernamePasswordAuthenticationFilter.class)
+			.sessionManagement((session) -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+		// .addFilterBefore(tokenExceptionFilter, TokenAuthenticationFilter.class);
 		return http.build();
 	}
+
 }
