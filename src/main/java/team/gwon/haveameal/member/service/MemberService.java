0bc74419b32117d87d19f8eb8a1ad25a8ea7f38e
wonjun <zkdl2347@naver.com>
2024-07-17 22:42:34 +0900

    (feat) : refresh token 저장 #EAT-141

--- src/main/java/team/gwon/haveameal/member/service/MemberService.java
+++ src/main/java/team/gwon/haveameal/member/service/MemberService.java
@@ -5,6 +5,8 @@ import java.io.IOException;
 import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
+import team.gwon.haveameal.common.domain.TokenDto;
+import team.gwon.haveameal.common.util.TokenProvider;
 import team.gwon.haveameal.member.converter.ToEntityConverter;
 import team.gwon.haveameal.member.converter.ToFindDtoConverter;
 import team.gwon.haveameal.member.domain.MemberEntity;
@@ -21,6 +23,7 @@ public class MemberService {
 	private final ToEntityConverter toEntityConverter;
 	private final ToFindDtoConverter toFindDtoConverter;
 	private final BCryptPasswordEncryptor passwordEncryptor;
+	private final TokenProvider tokenProvider;
 
 	public void insertMember(MemberRegisterDto memberDto) throws IOException {
 		MemberEntity memberEntity = toEntityConverter.toMemberEntity(memberDto);
@@ -32,11 +35,15 @@ public class MemberService {
 		return toFindDtoConverter.toMemberFindDto(memberEntity);
 	}
 
-	public boolean authenticate(String memberId, String password) {
+	public TokenDto authenticate(String memberId, String password) {
 		MemberEntity member = memberMapper.getMemberById(memberId);
-		if (member != null) {
-			return passwordEncryptor.matchPassword(password, member.getPassword());
+		if (member != null && passwordEncryptor.matchPassword(password, member.getPassword())) {
+			TokenDto tokenDto = tokenProvider.createAllToken(memberId);
+			//밑에 과정에서 NPE이나 exp 관련 의심해봐야함.
+			tokenProvider.insertRefreshToken(tokenDto.getRefreshToken());
+			return tokenDto;
+			//검증로직 탈 필요없이 refresh토큰을 redis에 저장시킨 후 반환.
 		}
-		return false;
+		return null;
 	}
 }
