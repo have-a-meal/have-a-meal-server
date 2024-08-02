ed0982a8cc6457aabdf40d4325895ef9f409137c
wonjun <zkdl2347@naver.com>
2024-08-02 18:21:51 +0900

    (refactor) : 토큰 검증을 컨텍스트 홀더에 있는지 검증 #EAT-141

--- src/main/java/team/gwon/haveameal/common/util/TokenAuthenticationFilter.java
+++ src/main/java/team/gwon/haveameal/common/util/TokenAuthenticationFilter.java
@@ -1,22 +1,20 @@
 package team.gwon.haveameal.common.util;
 
-import static org.springframework.http.MediaType.*;
-
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.Optional;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
-import io.jsonwebtoken.JwtException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import team.gwon.haveameal.common.domain.TokenDto;
 
 @RequiredArgsConstructor
 @Component
@@ -47,35 +45,49 @@ public class TokenAuthenticationFilter extends OncePerRequestFilter {
 		// 	filterChain.doFilter(request, response);
 		// 	return;
 		// }
-		log.info("doFilterInternal");
-		if (shouldNotFilter(request)) {
-			filterChain.doFilter(request, response);
-		}
+		// log.info("doFilterInternal");
+		// if (shouldNotFilter(request)) {
+		// 	filterChain.doFilter(request, response);
+		// }
+		// Optional<String> token = tokenProvider.extractToken(request);
+		// try {
+		// 	if (token.isPresent() && tokenProvider.tokenValidation(token.get())) {
+		// 		if (tokenProvider.getTokenType(token.get()).equals("refresh")) {
+		// 			log.info("ref-token: {}", token.get());
+		// 			//refresh 검증 먼저해줘야함.
+		// 			TokenDto tokenDto = tokenProvider.refreshTokenValidation(token.get());
+		// 			response.addHeader("Authorization", "Bearer " + tokenDto.getAccessToken());
+		// 			response.addHeader("Refresh-Token", "Bearer " + tokenDto.getRefreshToken());
+		// 		} else {
+		// 			log.info("acc-token: {}", token.get());
+		// 		}
+		// 		// filterChain.doFilter(request, response);
+		// 	} else {
+		// 		throw new JwtException("토큰 값이 없습니다.");
+		// 		// log.info("토큰이 없습니다.");
+		// 	}
+		// } catch (JwtException e) {
+		// 	log.error(e.getMessage(), e);
+		// 	response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+		// 	response.setContentType(APPLICATION_JSON_VALUE);
+		// 	response.setCharacterEncoding("UTF-8");
+		// 	response.getWriter().write(e.getMessage());
+		// 	return;
+		// }
+		// filterChain.doFilter(request, response);
+		//=====================================================
 		Optional<String> token = tokenProvider.extractToken(request);
-		try {
-			if (token.isPresent() && tokenProvider.tokenValidation(token.get())) {
-				if (tokenProvider.getTokenType(token.get()).equals("refresh")) {
-					log.info("ref-token: {}", token.get());
-					//refresh 검증 먼저해줘야함.
-					TokenDto tokenDto = tokenProvider.refreshTokenValidation(token.get());
-					response.addHeader("Authorization", "Bearer " + tokenDto.getAccessToken());
-					response.addHeader("Refresh-Token", "Bearer " + tokenDto.getRefreshToken());
-				} else {
-					log.info("acc-token: {}", token.get());
-				}
-				filterChain.doFilter(request, response);
-			} else {
-				// throw new JwtException("토큰 값이 없습니다.");
-				log.info("토큰이 없습니다.");
-			}
-		} catch (JwtException e) {
-			log.error(e.getMessage(), e);
-			response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-			response.setContentType(APPLICATION_JSON_VALUE);
-			response.setCharacterEncoding("UTF-8");
-			response.getWriter().write(e.getMessage());
-			return;
+		log.info("[doFilterInternal] token 값 추출 완료. token : {}", token);
+
+		log.info("[doFilterInternal] token 값 유효성 체크 시작");
+		if (token.isPresent() && tokenProvider.tokenValidation(token.get())) {
+			Authentication authentication = tokenProvider.getAuthentication(token.get());
+			SecurityContextHolder.getContext().setAuthentication(authentication);
+			log.info("[doFilterInternal] token 값 유효성 체크 완료");
+		} else {
+			log.info("[doFilterInternal] token 값 유효성 체크 실패");
 		}
+
 		filterChain.doFilter(request, response);
 	}
 }
